bdb81867a93b6bddae83d5d82a15a546cd44b702
Moe0088 <you@example.com>
2024-07-12 20:39:32 -0400

    s2

--- src/com/codercampus/com/RunnableApplication.java
+++ src/com/codercampus/com/RunnableApplication.java
@@ -1,8 +1,9 @@
+package com.codercampus.com;
 
 public class RunnableApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("Hello");
 
 	}
 
